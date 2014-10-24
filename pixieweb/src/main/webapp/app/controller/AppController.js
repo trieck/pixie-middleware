70218a87ef5155a8327f71822103878bd8cfc8c6
trieck <trieck@gmail.com>
2014-10-23 21:11:01 -0400

    Incremental

--- pixieweb/src/main/webapp/app/controller/AppController.js
+++ pixieweb/src/main/webapp/app/controller/AppController.js
@@ -1,6 +1,14 @@
 Ext.define('pixieweb.controller.AppController', {
-	extend: 'Ext.app.Controller',
-	init: function () {
-		this.control({});
-	}
+    extend: 'Ext.app.Controller',
+    init: function () {
+        this.control({
+            "#searchText": {
+                triggerclick: this.onTriggerClick
+            }
+        });
+    },
+
+    onTriggerClick: function (trigger, event) {
+        Ext.Msg.alert(trigger.getValue());
+    }
 });
--- pixieweb/src/main/webapp/app/lib/Viewport.js
+++ pixieweb/src/main/webapp/app/lib/Viewport.js
@@ -1,46 +1,55 @@
 Ext.define('pixieweb.lib.Viewport', {
-	extend: 'Ext.Viewport',
-	alias: 'pixielib-viewport',
-	title: 'Pixie Search Engine',
-	layout: 'border',
-	renderTo: Ext.getBody(),
-	defaults: {
-		split: true
-	},
+    extend: 'Ext.Viewport',
+    alias: 'pixielib-viewport',
+    title: 'Pixie Search Engine',
+    layout: 'border',
+    defaults: {
+        split: true
+    },
 
-	items: [{
-		region: 'north',
-		html: '<h1 class="x-panel-header">Pixie Search Engine</h1>',
-		border: false
-	}, {
-		region: 'west',
-		collapsible: true,
-		title: 'Search',
-		width: 200
-	}, {
-		region: 'south',
-		title: 'Log',
-		collapsible: true,
-		split: true,
-		height: 100,
-		minHeight: 100
-	}, {
-		region: 'east',
-		title: 'Properties',
-		collapsible: true,
-		split: true,
-		width: 150
-	}, {
-		region: 'center',
-		xtype: 'tabpanel',
-		activeTab: 0,
-		items: {
-			title: 'Results'
-		}
-	}],
+    items: [
+        {
+            region: 'west',
+            collapsible: true,
+            title: 'Bible Search Engine',
+            width: 200,
+            border: true,
+            tbar: [
+                {
+                    padding: '5',
+                    id: 'searchText',
+                    name: 'search',
+                    width: 180,
+                    emptyText: 'Enter search query...',
+                    xtype: 'trigger',
+                    trigger1Cls: 'x-form-clear-trigger',
+                    trigger2Cls: 'x-form-search-trigger',
+                    onTriggerClick: function (e) {
+                        var me = this;
+                        me.fireEvent("triggerclick", me, e);
+                    }
+                }
+            ]
+        },
+        {
+            region: 'east',
+            title: 'Details',
+            collapsible: true,
+            split: true,
+            width: 150
+        }, {
+            region: 'center',
+            xtype: 'tabpanel',
+            activeTab: 0,
+            items: [{
+                title: 'Summary'
+            }]
+        }
+    ],
 
-	initComponent: function () {
-		Ext.QuickTips.init();
-		this.callParent(arguments);
-	}
-});
+    initComponent: function () {
+        Ext.QuickTips.init();
+        this.callParent(arguments);
+    }
+})
+;
