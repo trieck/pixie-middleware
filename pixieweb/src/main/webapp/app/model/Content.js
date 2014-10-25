8dc22f9727761921fbfb732df79a1d006b51e660
trieck <trieck@gmail.com>
2014-10-25 18:25:35 -0400

    Convert highlight tags to HTML

--- pixieweb/src/main/webapp/app/model/Content.js
+++ pixieweb/src/main/webapp/app/model/Content.js
@@ -1,4 +1,11 @@
 Ext.define('pixieweb.model.Content', {
     extend: 'Ext.data.Model',
-    fields: [ 'book', 'chapter', 'verse', 'text' ]
+    fields: [ 'book', 'chapter', 'verse', {
+        name: 'text', convert: function (value) {
+            // highlight
+            value = value.replace(/<highlight>/, "<strong>");
+            value = value.replace(/<\/highlight>/, "</strong>");
+            return value;
+        }
+    } ]
 });
