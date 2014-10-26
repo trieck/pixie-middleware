dccc3296cf8a39a02c8aa25af97c601ebc28f49e
trieck <trieck@gmail.com>
2014-10-26 16:21:03 -0400

    Dedicated middle-ware

--- pixieweb/src/main/webapp/app.js
+++
@@ -1,9 +0,0 @@
-Ext.application({
-	name: 'pixieweb',
-	appFolder: 'app',
-	controllers: ['AppController'],
-	uses: 'pixieweb.lib.Viewport',
-	launch: function () {
-		Ext.create('pixielib-viewport');
-	}
-});
